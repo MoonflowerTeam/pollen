f098653e706459c4814d37ba8430bfcdca424092
Jackson <jackson.hardaway@gmail.com>
2021-11-05 17:41:42 -0700

    Added platform registry

---
+++ forge/src/main/java/gg/moonflower/pollen/api/registry/forge/PollinatedRegistryImpl.java
@@ -0,0 +1,48 @@
+package gg.moonflower.pollen.api.registry.forge;
+
+import gg.moonflower.pollen.api.platform.PlatformInstance;
+import gg.moonflower.pollen.api.platform.forge.ForgePlatformInstance;
+import gg.moonflower.pollen.api.registry.PollinatedRegistry;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+import net.minecraftforge.registries.RegistryManager;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
+
+@ApiStatus.Internal
+public class PollinatedRegistryImpl<T extends IForgeRegistryEntry<T>> extends PollinatedRegistry<T> {
+    private final DeferredRegister<T> registry;
+
+    private PollinatedRegistryImpl(ForgeRegistry<T> registry, String modId) {
+        super(modId);
+        this.registry = DeferredRegister.create(registry, modId);
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public static <T> PollinatedRegistry<T> create(Registry<T> registry, String modId) {
+        ForgeRegistry forgeRegistry = getForgeRegistry((ResourceKey) registry.key());
+        if (forgeRegistry == null)
+            return PollinatedRegistry.createVanilla(registry, modId);
+        return new PollinatedRegistryImpl(forgeRegistry, modId);
+    }
+
+    @Nullable
+    private static <T extends IForgeRegistryEntry<T>> ForgeRegistry<T> getForgeRegistry(ResourceKey<Registry<T>> registryKey) {
+        return RegistryManager.ACTIVE.getRegistry(registryKey);
+    }
+
+    @Override
+    public <I extends T> Supplier<I> register(String id, Supplier<I> object) {
+        return this.registry.register(id, object);
+    }
+
+    @Override
+    protected void onRegister(PlatformInstance mod) {
+        this.registry.register(((ForgePlatformInstance) mod).getEventBus());
+    }
+}
