4969e84f42b73a4977cacb2adf796d6ed3eb60b0
Jackson <jackson.hardaway@gmail.com>
2021-11-06 11:16:21 -0700

    Fixed impl names

--- forge/src/main/java/gg/moonflower/pollen/api/platform/forge/PlatformBuilderImpl.java
+++ forge/src/main/java/gg/moonflower/pollen/api/platform/forge/PlatformBuilderImpl.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.jetbrains.annotations.ApiStatus;
 
 @ApiStatus.Internal
-public final class PlatformInstanceBuilderImpl {
+public final class PlatformBuilderImpl {
 
     public static Platform buildImpl(String modId, Runnable commonInit, Runnable clientInit, Runnable commonPostInit, Runnable clientPostInit, Runnable commonNetworkInit, Runnable clientNetworkInit) {
         return new ForgePlatform(modId, FMLJavaModLoadingContext.get().getModEventBus(), commonInit, clientInit, commonPostInit, clientPostInit, commonNetworkInit, clientNetworkInit);
