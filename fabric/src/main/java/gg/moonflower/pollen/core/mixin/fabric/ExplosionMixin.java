8bcd9351f2dc5434d6d5461ca6732e5d97460fe4
abigailfails <thatvaliantvole@gmail.com>
2021-12-14 14:13:52 +0000

    Made ExplosionMixin use shadow

--- fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/ExplosionMixin.java
+++ fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/ExplosionMixin.java
@@ -5,8 +5,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,12 +17,13 @@ import java.util.List;
 import java.util.Set;
 
 @Mixin(Explosion.class)
-public abstract class ExplosionMixin {
-    @Accessor("level")
-    abstract Level getLevel();
+public class ExplosionMixin {
+    @Final
+    @Shadow
+    private Level level;
 
     @Inject(method = "explode", at = @At(value = "NEW", target = "net/minecraft/world/phys/Vec3", ordinal = 0, shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
     public void explode(CallbackInfo ci, Set<BlockPos> set, int i, float q, int k, int l, int r, int s, int t, int u, List<Entity> list) {
-        ExplosionEvents.DETONATE.invoker().detonate(getLevel(), (Explosion) (Object) this, list);
+        ExplosionEvents.DETONATE.invoker().detonate(level, (Explosion) (Object) this, list);
     }
 }
