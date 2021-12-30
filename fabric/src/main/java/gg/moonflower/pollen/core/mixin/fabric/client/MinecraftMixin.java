1e025b7f3aaf5c0fb396e290224815d0fb9771a7
Ocelot <FinntheRaider@gmail.com>
2021-12-30 15:24:03 -0700

    Fixed Forge crash on Minecraft mixin

--- fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/client/MinecraftMixin.java
+++ fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/client/MinecraftMixin.java
@@ -1,4 +1,4 @@
-package gg.moonflower.pollen.core.mixin.client;
+package gg.moonflower.pollen.core.mixin.fabric.client;
 
 import gg.moonflower.pollen.api.event.events.client.render.InitRendererEvent;
 import net.minecraft.client.Minecraft;
--- forge/src/main/java/gg/moonflower/pollen/core/forge/PollenForge.java
+++ forge/src/main/java/gg/moonflower/pollen/core/forge/PollenForge.java
@@ -1,7 +1,11 @@
 package gg.moonflower.pollen.core.forge;
 
+import gg.moonflower.pollen.api.event.events.client.render.InitRendererEvent;
 import gg.moonflower.pollen.api.sync.forge.SyncedDataManagerImpl;
 import gg.moonflower.pollen.core.Pollen;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -13,7 +17,9 @@ public class PollenForge {
 
     public PollenForge() {
         Pollen.PLATFORM.setup();
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(PollenForge::init);
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modBus.addListener(PollenForge::init);
+        modBus.addListener(EventPriority.NORMAL, true, ColorHandlerEvent.Block.class, event -> InitRendererEvent.EVENT.invoker().initRenderer());
     }
 
     private static void init(FMLCommonSetupEvent event) {
