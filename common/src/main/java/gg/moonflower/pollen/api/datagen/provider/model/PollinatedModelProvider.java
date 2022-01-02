f3b20c59434c83c417d3b8363ae1596ecdea3a57
Ocelot <FinntheRaider@gmail.com>
2022-01-02 13:17:19 -0700

    Added model provider

---
+++ common/src/main/java/gg/moonflower/pollen/api/datagen/provider/model/PollinatedModelProvider.java
@@ -0,0 +1,142 @@
+package gg.moonflower.pollen.api.datagen.provider.model;
+
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import gg.moonflower.pollen.api.util.PollinatedModContainer;
+import net.minecraft.core.Registry;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
+import net.minecraft.data.HashCache;
+import net.minecraft.data.models.blockstates.BlockStateGenerator;
+import net.minecraft.data.models.model.DelegatedModel;
+import net.minecraft.data.models.model.ModelLocationUtils;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+/**
+ * Generates models for items and blocks, as well as block states. Generators can be added using {@link PollinatedModelGenerator}.
+ *
+ * @author Ocelot
+ * @see PollinatedModelGenerator
+ * @see PollinatedBlockModelGenerator
+ * @see PollinatedItemModelGenerator
+ * @since 1.0.0
+ */
+public abstract class PollinatedModelProvider implements DataProvider {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+    private final Set<ModelGeneratorFactory> factories;
+    private final DataGenerator generator;
+    private final String domain;
+
+    public PollinatedModelProvider(DataGenerator generator, PollinatedModContainer container) {
+        this.factories = new HashSet<>();
+        this.generator = generator;
+        this.domain = container.getId();
+    }
+
+    /**
+     * Adds a generator for creating models and block states.
+     *
+     * @param factory The factory to the provider
+     */
+    public PollinatedModelProvider addGenerator(ModelGeneratorFactory factory) {
+        this.factories.add(factory);
+        return this;
+    }
+
+    @Override
+    public void run(HashCache cache) {
+        Path path = this.generator.getOutputFolder();
+
+        Map<Block, BlockStateGenerator> blockStates = new HashMap<>();
+        Consumer<BlockStateGenerator> blockStateOutput = blockStateGenerator -> {
+            Block block = blockStateGenerator.getBlock();
+            BlockStateGenerator blockState = blockStates.put(block, blockStateGenerator);
+            if (blockState != null)
+                throw new IllegalStateException("Duplicate blockstate definition for " + block);
+        };
+        Map<ResourceLocation, Supplier<JsonElement>> models = Maps.newHashMap();
+        Set<Item> skippedAutoModels = new HashSet<>();
+        BiConsumer<ResourceLocation, Supplier<JsonElement>> modelOutput = (resourceLocation, supplier) -> {
+            Supplier<JsonElement> model = models.put(resourceLocation, supplier);
+            if (model != null)
+                throw new IllegalStateException("Duplicate model definition for " + resourceLocation);
+        };
+        Consumer<Item> skippedAutoModelsOutput = skippedAutoModels::add;
+
+        this.factories.stream().map(factory -> factory.create(blockStateOutput, modelOutput, skippedAutoModelsOutput)).forEach(PollinatedModelGenerator::run);
+
+        List<Block> blocks = Registry.BLOCK.stream().filter(block -> this.domain.equals(Registry.BLOCK.getKey(block).getNamespace()) && !blockStates.containsKey(block)).collect(Collectors.toList());
+        if (!blocks.isEmpty())
+            throw new IllegalStateException("Missing blockstate definitions for: " + blocks);
+
+        Registry.BLOCK.forEach(block -> {
+            if (!this.domain.equals(Registry.BLOCK.getKey(block).getNamespace()))
+                return;
+
+            Item item = Item.BY_BLOCK.get(block);
+            if (item != null) {
+                if (skippedAutoModels.contains(item))
+                    return;
+
+                ResourceLocation itemLocation = ModelLocationUtils.getModelLocation(item);
+                if (!models.containsKey(itemLocation))
+                    models.put(itemLocation, new DelegatedModel(ModelLocationUtils.getModelLocation(block)));
+            }
+        });
+        this.saveCollection(cache, path, blockStates, PollinatedModelProvider::createBlockStatePath);
+        this.saveCollection(cache, path, models, PollinatedModelProvider::createModelPath);
+    }
+
+    private <T> void saveCollection(HashCache cache, Path rootPath, Map<T, ? extends Supplier<JsonElement>> objectToJsonMap, BiFunction<Path, T, Path> resolveObjectPath) {
+        objectToJsonMap.forEach((object, supplier) -> {
+            Path path = resolveObjectPath.apply(rootPath, object);
+
+            try {
+                DataProvider.save(GSON, cache, supplier.get(), path);
+            } catch (Exception var7) {
+                LOGGER.error("Couldn't save {}", path, var7);
+            }
+        });
+    }
+
+    private static Path createBlockStatePath(Path rootPath, Block block) {
+        ResourceLocation resourceLocation = Registry.BLOCK.getKey(block);
+        return rootPath.resolve("assets/" + resourceLocation.getNamespace() + "/blockstates/" + resourceLocation.getPath() + ".json");
+    }
+
+    private static Path createModelPath(Path rootPath, ResourceLocation modelLocation) {
+        return rootPath.resolve("assets/" + modelLocation.getNamespace() + "/models/" + modelLocation.getPath() + ".json");
+    }
+
+    @Override
+    public String getName() {
+        return "Block State Definitions";
+    }
+
+    /**
+     * Factory for creating new generators for models and block states.
+     *
+     * @author Ocelot
+     * @since 1.0.0
+     */
+    @FunctionalInterface
+    public interface ModelGeneratorFactory {
+        PollinatedModelGenerator create(Consumer<BlockStateGenerator> blockStateOutput, BiConsumer<ResourceLocation, Supplier<JsonElement>> modelOutput, Consumer<Item> skippedAutoModelsOutput);
+    }
+}
