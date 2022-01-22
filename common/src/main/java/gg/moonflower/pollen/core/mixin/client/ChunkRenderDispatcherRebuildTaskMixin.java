77aa01bad890245979ff75407f4e43565ae2b3c2
Ocelot <FinntheRaider@gmail.com>
2022-01-22 15:04:02 -0700

    Fixed crash when loading chunks in 1.18

--- common/src/main/java/gg/moonflower/pollen/core/mixin/client/ChunkRenderDispatcherRebuildTaskMixin.java
+++ common/src/main/java/gg/moonflower/pollen/core/mixin/client/ChunkRenderDispatcherRebuildTaskMixin.java
@@ -7,13 +7,12 @@ import gg.moonflower.pollen.pinwheel.api.client.render.TickableBlockRenderer;
 import net.minecraft.client.renderer.ChunkBufferBuilderPack;
 import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
 import net.minecraft.client.renderer.chunk.RenderChunkRegion;
+import net.minecraft.client.renderer.chunk.VisGraph;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -25,22 +24,22 @@ import java.util.Set;
 @Mixin(targets = "net/minecraft/client/renderer/chunk/ChunkRenderDispatcher$RenderChunk$RebuildTask")
 public class ChunkRenderDispatcherRebuildTaskMixin {
 
-    @Shadow
-    @Nullable
-    protected RenderChunkRegion region;
-
-    @Inject(method = "compile", at = @At(value = "INVOKE_ASSIGN", target = "Lcom/google/common/collect/Sets;newHashSet()Ljava/util/HashSet;", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
-    public void compile(float x, float y, float z, ChunkRenderDispatcher.CompiledChunk chunk, ChunkBufferBuilderPack pack, CallbackInfoReturnable<Set<BlockEntity>> cir, int i, BlockPos originPos, BlockPos offset) {
-        if (this.region != null) {
-            for (BlockPos pos : BlockPos.betweenClosed(originPos, offset)) {
-                BlockState state = this.region.getBlockState(pos);
-                if (state.isAir())
-                    continue;
-                BlockRenderer renderer = BlockRendererRegistry.getFirst(state.getBlock());
-                if (renderer != null && renderer.getRenderShape(state) != RenderShape.MODEL)
-                    ((CompiledChunkExtension) chunk).pollen_getTickingBlockRenderers().add(pos.immutable());
-                if (BlockRendererRegistry.get(state.getBlock()).stream().anyMatch(r -> r instanceof TickableBlockRenderer))
-                    ((CompiledChunkExtension) chunk).pollen_getTickingBlockRenderers().add(pos.immutable());
+    @Inject(method = "compile", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;<init>()V", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void compile(float x, float y, float z, ChunkRenderDispatcher.CompiledChunk chunk, ChunkBufferBuilderPack pack, CallbackInfoReturnable<Set<BlockEntity>> cir, int i, BlockPos originPos, BlockPos offset, VisGraph visGraph, Set<BlockEntity> set, RenderChunkRegion region) {
+        if (region != null) {
+            try {
+                for (BlockPos pos : BlockPos.betweenClosed(originPos, offset)) {
+                    BlockState state = region.getBlockState(pos);
+                    if (state.isAir())
+                        continue;
+                    BlockRenderer renderer = BlockRendererRegistry.getFirst(state.getBlock());
+                    if (renderer != null && renderer.getRenderShape(state) != RenderShape.MODEL)
+                        ((CompiledChunkExtension) chunk).pollen_getTickingBlockRenderers().add(pos.immutable());
+                    if (BlockRendererRegistry.get(state.getBlock()).stream().anyMatch(r -> r instanceof TickableBlockRenderer))
+                        ((CompiledChunkExtension) chunk).pollen_getTickingBlockRenderers().add(pos.immutable());
+                }
+            } catch (NullPointerException t) {
+                t.printStackTrace(); // just in case
             }
         }
     }
