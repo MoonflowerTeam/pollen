d17f6eac4c9696575271ffeda76206df8d9662cc
Ocelot <FinntheRaider@gmail.com>
2022-01-22 15:20:51 -0700

    Added the ability to reload geometry through debug keys

--- common/src/main/java/gg/moonflower/pollen/core/DebugInputs.java
+++ common/src/main/java/gg/moonflower/pollen/core/DebugInputs.java
@@ -1,11 +1,19 @@
 package gg.moonflower.pollen.core;
 
+import com.google.common.base.Stopwatch;
 import gg.moonflower.pollen.api.client.util.TextureDownloader;
 import gg.moonflower.pollen.api.event.events.client.InputEvents;
 import gg.moonflower.pollen.api.platform.Platform;
+import gg.moonflower.pollen.pinwheel.api.client.animation.AnimationManager;
+import gg.moonflower.pollen.pinwheel.api.client.geometry.GeometryModelManager;
+import gg.moonflower.pollen.pinwheel.api.client.texture.GeometryTextureManager;
+import net.minecraft.ChatFormatting;
 import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.ChatScreen;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.ApiStatus;
 import org.lwjgl.glfw.GLFW;
 
@@ -32,6 +40,21 @@ public class DebugInputs {
         if (Platform.isProduction() || Minecraft.getInstance().screen instanceof ChatScreen || action != GLFW.GLFW_PRESS)
             return false;
         if (key == GLFW.GLFW_KEY_BACKSLASH) {
+            Player player = Minecraft.getInstance().player;
+            if ((modifiers & GLFW.GLFW_MOD_ALT) > 0) {
+                if (player == null)
+                    return true;
+
+                final Stopwatch timer = Stopwatch.createStarted();
+                CompletableFuture.allOf(GeometryModelManager.reload(false), GeometryTextureManager.reload(false), AnimationManager.reload(false)).handle((u, t) -> {
+                    timer.stop();
+                    Minecraft.getInstance().gui.getChat().addMessage(new TextComponent("").append(new TranslatableComponent("debug.prefix").withStyle(ChatFormatting.YELLOW, ChatFormatting.BOLD)).append(" Took " + timer + " to reload geometry models, textures, and animations"));
+                    return u;
+                });
+
+                return true;
+            }
+
             try {
                 Path outputFolder = Paths.get(Minecraft.getInstance().gameDirectory.toURI()).resolve("debug-out");
                 if (!Files.exists(outputFolder)) {
