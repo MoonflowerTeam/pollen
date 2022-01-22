5a777663a4092b3326d896be3399a10d8016dd9a
Ocelot <FinntheRaider@gmail.com>
2022-01-22 12:26:41 -0700

    Fixed 1.18 changes

--- fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/client/EntityRenderDispatcherMixin.java
+++ fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/client/EntityRenderDispatcherMixin.java
@@ -1,20 +1,17 @@
 package gg.moonflower.pollen.core.mixin.fabric.client;
 
 import gg.moonflower.pollen.api.event.events.client.render.AddRenderLayersEvent;
-import net.minecraft.client.Options;
-import net.minecraft.client.gui.Font;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.client.renderer.entity.player.PlayerRenderer;
-import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.server.packs.resources.ReloadableResourceManager;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -27,16 +24,14 @@ import java.util.Set;
 @Mixin(EntityRenderDispatcher.class)
 public abstract class EntityRenderDispatcherMixin {
 
-    @Final
     @Shadow
-    private Map<String, PlayerRenderer> playerRenderers;
+    private Map<String, EntityRenderer<? extends Player>> playerRenderers;
 
-    @Final
     @Shadow
     private Map<EntityType<?>, EntityRenderer<?>> renderers;
 
-    @Inject(method = "<init>", at = @At("TAIL"))
-    public void init(TextureManager textureManager, ItemRenderer itemRenderer, ReloadableResourceManager reloadableResourceManager, Font font, Options options, CallbackInfo ci) {
+    @Inject(method = "onResourceManagerReload", at = @At("TAIL"))
+    public void onResourceManagerReload(ResourceManager resourceManager, CallbackInfo ci) {
         AddRenderLayersEvent.EVENT.invoker().addLayers(new AddRenderLayersEvent.Context() {
             @Override
             public Set<String> getSkins() {
@@ -45,7 +40,7 @@ public abstract class EntityRenderDispatcherMixin {
 
             @Nullable
             @Override
-            public PlayerRenderer getSkin(String skinName) {
+            public EntityRenderer<? extends Player> getSkin(String skinName) {
                 return playerRenderers.get(skinName);
             }
 
@@ -53,6 +48,11 @@ public abstract class EntityRenderDispatcherMixin {
             @Override
             public <T extends LivingEntity, R extends LivingEntityRenderer<T, ? extends EntityModel<T>>> @Nullable R getRenderer(EntityType<? extends T> entityType) {
                 return (R) renderers.get(entityType);
+            }
+
+            @Override
+            public EntityModelSet getEntityModels() {
+                return Minecraft.getInstance().getEntityModels();
             }
         });
     }
