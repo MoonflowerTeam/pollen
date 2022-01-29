935493033e0eec0b5379722bb636f99ef9454489
Jackson <jackson.hardaway@gmail.com>
2022-01-29 14:27:42 -0800

    Fixed models not registering on Forge

--- forge/src/main/java/gg/moonflower/pollen/api/registry/client/forge/ModelRegistryImpl.java
+++ forge/src/main/java/gg/moonflower/pollen/api/registry/client/forge/ModelRegistryImpl.java
@@ -1,15 +1,19 @@
 package gg.moonflower.pollen.api.registry.client.forge;
 
+import gg.moonflower.pollen.core.Pollen;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.ApiStatus;
 
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 @ApiStatus.Internal
+@Mod.EventBusSubscriber(modid = Pollen.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModelRegistryImpl {
 
     private static final Set<ResourceLocation> SPECIAL_MODELS = ConcurrentHashMap.newKeySet();
