5a4b6d4052c1d8e79f75016c388e9487c6f7f848
Ocelot <FinntheRaider@gmail.com>
2022-02-18 16:32:30 -0700

    Fixed 1.18 errors

--- common/src/main/java/gg/moonflower/pollen/core/mixin/client/ChunkRenderDispatcherRebuildTaskMixin.java
+++ common/src/main/java/gg/moonflower/pollen/core/mixin/client/ChunkRenderDispatcherRebuildTaskMixin.java
@@ -7,12 +7,14 @@ import gg.moonflower.pollen.pinwheel.api.client.render.TickableBlockRenderer;
 import net.minecraft.client.renderer.ChunkBufferBuilderPack;
 import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
 import net.minecraft.client.renderer.chunk.RenderChunkRegion;
-import net.minecraft.client.renderer.chunk.VisGraph;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -24,12 +26,24 @@ import java.util.Set;
 @Mixin(targets = "net/minecraft/client/renderer/chunk/ChunkRenderDispatcher$RenderChunk$RebuildTask")
 public class ChunkRenderDispatcherRebuildTaskMixin {
 
+    @Unique
+    private RenderChunkRegion captureRegion;
+
+    @Shadow
+    @Nullable
+    protected RenderChunkRegion region;
+
+    @Inject(method = "compile", at = @At("HEAD"))
+    public void captureRegion(float f, float g, float h, ChunkRenderDispatcher.CompiledChunk compiledChunk, ChunkBufferBuilderPack chunkBufferBuilderPack, CallbackInfoReturnable<Set<BlockEntity>> cir) {
+        this.captureRegion = this.region;
+    }
+
     @Inject(method = "compile", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;<init>()V", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
-    public void compile(float x, float y, float z, ChunkRenderDispatcher.CompiledChunk chunk, ChunkBufferBuilderPack pack, CallbackInfoReturnable<Set<BlockEntity>> cir, int i, BlockPos originPos, BlockPos offset, VisGraph visGraph, Set<BlockEntity> set, RenderChunkRegion region) {
-        if (region != null) {
+    public void compile(float x, float y, float z, ChunkRenderDispatcher.CompiledChunk chunk, ChunkBufferBuilderPack pack, CallbackInfoReturnable<Set<BlockEntity>> cir, int i, BlockPos originPos) {
+        if (this.captureRegion != null) {
             try {
-                for (BlockPos pos : BlockPos.betweenClosed(originPos, offset)) {
-                    BlockState state = region.getBlockState(pos);
+                for (BlockPos pos : BlockPos.betweenClosed(originPos, originPos.offset(15, 15, 15))) {
+                    BlockState state = this.captureRegion.getBlockState(pos);
                     if (state.isAir())
                         continue;
                     BlockRenderer renderer = BlockRendererRegistry.getFirst(state.getBlock());
