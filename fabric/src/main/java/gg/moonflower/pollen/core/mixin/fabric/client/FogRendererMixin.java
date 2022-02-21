ce3a9c9315add33f2529dbc0ef003b28d130d2b8
Ocelot <FinntheRaider@gmail.com>
2022-02-21 15:20:18 -0700

    Fixed Optifine breaking fog renderer mixin

--- fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/client/FogRendererMixin.java
+++ fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/client/FogRendererMixin.java
@@ -67,7 +67,7 @@ public class FogRendererMixin {
         }, partialTicks);
     }
 
-    @Inject(method = "setupFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setupNvFogDistance()V", shift = At.Shift.AFTER))
+    @Inject(method = "setupFog", at = @At("TAIL"))
     private static void modifyFogDensity(Camera camera, FogRenderer.FogMode fogType, float farPlaneDistance, boolean nearFog, CallbackInfo ci) {
         FogEvents.FOG_DENSITY.invoker().setupFogDensity(Minecraft.getInstance().gameRenderer, camera, CONTEXT, farPlaneDistance, capturePartialTicks);
     }
