684b5a9f2fdedd4e071451a33a1edab185825088
Ocelot <FinntheRaider@gmail.com>
2022-03-05 16:57:35 -0700

    Added model factories

--- fabric/src/main/java/gg/moonflower/pollen/api/registry/client/fabric/ModelRegistryImpl.java
+++ fabric/src/main/java/gg/moonflower/pollen/api/registry/client/fabric/ModelRegistryImpl.java
@@ -1,5 +1,6 @@
 package gg.moonflower.pollen.api.registry.client.fabric;
 
+import gg.moonflower.pollen.api.registry.client.ModelRegistry;
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.ApiStatus;
@@ -8,5 +9,9 @@ import org.jetbrains.annotations.ApiStatus;
 public class ModelRegistryImpl {
     public static void registerSpecial(ResourceLocation location) {
         ModelLoadingRegistry.INSTANCE.registerModelProvider((manager, out) -> out.accept(location));
+    }
+
+    public static void registerFactory(ModelRegistry.ModelFactory factory) {
+        ModelLoadingRegistry.INSTANCE.registerModelProvider(factory::registerModels);
     }
 }
