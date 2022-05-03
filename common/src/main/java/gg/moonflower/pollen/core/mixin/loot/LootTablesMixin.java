42647dcea07321a560e1c2053d73c7403617590a
Ocelot <FinntheRaider@gmail.com>
2022-05-02 21:04:52 -0600

    Added a null check for server future. Fixes #30

--- common/src/main/java/gg/moonflower/pollen/core/mixin/loot/LootTablesMixin.java
+++ common/src/main/java/gg/moonflower/pollen/core/mixin/loot/LootTablesMixin.java
@@ -8,12 +8,20 @@ import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.level.storage.loot.LootTables;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @Mixin(LootTables.class)
 public abstract class LootTablesMixin extends SimpleJsonResourceReloadListener {
+
+    @Shadow
+    @Final
+    private static Logger LOGGER;
 
     private LootTablesMixin(Gson gson, String string) {
         super(gson, string);
@@ -22,7 +30,13 @@ public abstract class LootTablesMixin extends SimpleJsonResourceReloadListener {
     @Override
     protected Map<ResourceLocation, JsonElement> prepare(ResourceManager resourceManager, ProfilerFiller profiler) {
         Map<ResourceLocation, JsonElement> object = super.prepare(resourceManager, profiler);
-        ResourceModifierManager.getServerCompleteFuture().join(); // Wait for server modifiers before completing
+        CompletableFuture<Void> completeFuture = ResourceModifierManager.getServerCompleteFuture();
+        if (completeFuture != null) {
+            completeFuture.join(); // Wait for server modifiers before completing
+        } else {
+            LOGGER.warn("Expected to wait for resource modifiers, but there was no pending future.");
+        }
+
         return object;
     }
 }
