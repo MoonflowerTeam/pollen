6b31097f6165f5498eca90addda06ec74467bc48
Jackson <jackson.hardaway@gmail.com>
2022-06-04 02:15:14 -0700

    Fixed mixin incompatibility with Mixin Extras

--- common/src/main/java/gg/moonflower/pollen/core/mixin/ServerResourcesMixin.java
+++ common/src/main/java/gg/moonflower/pollen/core/mixin/ServerResourcesMixin.java
@@ -1,12 +1,11 @@
 package gg.moonflower.pollen.core.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import gg.moonflower.pollen.api.resource.modifier.ResourceModifierManager;
 import net.minecraft.server.ReloadableServerResources;
 import net.minecraft.server.packs.resources.PreparableReloadListener;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +13,10 @@ import java.util.List;
 @Mixin(ReloadableServerResources.class)
 public class ServerResourcesMixin {
 
-    @Inject(method = "listeners", at = @At("TAIL"), cancellable = true)
-    public void addListeners(CallbackInfoReturnable<List<PreparableReloadListener>> cir) {
-        List<PreparableReloadListener> list = new ArrayList<>(cir.getReturnValue());
+    @ModifyReturnValue(method = "listeners", at = @At("RETURN"))
+    public List<PreparableReloadListener> addListeners(List<PreparableReloadListener> listeners) {
+        List<PreparableReloadListener> list = new ArrayList<>(listeners);
         list.add(0, ResourceModifierManager.createServerReloader((ReloadableServerResources) (Object) this));
-        cir.setReturnValue(list);
+        return list;
     }
 }
