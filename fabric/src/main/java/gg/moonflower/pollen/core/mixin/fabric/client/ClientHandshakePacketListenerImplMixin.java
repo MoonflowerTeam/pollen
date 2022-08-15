311ce589b046bc548eaa210c7c6346ea97aabb73
Ocelot <FinntheRaider@gmail.com>
2022-08-15 11:25:32 -0600

    Configs are now synced after dummy configs are loaded

--- fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/client/ClientHandshakePacketListenerImplMixin.java
+++ fabric/src/main/java/gg/moonflower/pollen/core/mixin/fabric/client/ClientHandshakePacketListenerImplMixin.java
@@ -3,10 +3,12 @@ package gg.moonflower.pollen.core.mixin.fabric.client;
 import gg.moonflower.pollen.api.config.fabric.ConfigTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientHandshakePacketListenerImpl;
+import net.minecraft.network.protocol.login.ClientboundCustomQueryPacket;
 import net.minecraft.network.protocol.login.ClientboundGameProfilePacket;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,10 +19,14 @@ public class ClientHandshakePacketListenerImplMixin {
     @Shadow
     @Final
     private Minecraft minecraft;
+    @Unique
+    private boolean loadedServerConfigs;
 
-    @Inject(method = "handleGameProfile", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/Connection;setListener(Lnet/minecraft/network/PacketListener;)V", shift = At.Shift.BEFORE))
-    public void handleGameProfile(ClientboundGameProfilePacket clientboundGameProfilePacket, CallbackInfo ci) {
-        if (!this.minecraft.hasSingleplayerServer()) // Don't override actual configs with client dummy data for the local player
+    @Inject(method = "handleCustomQuery", at = @At("HEAD"))
+    public void handleCustomQuery(ClientboundCustomQueryPacket packet, CallbackInfo ci) {
+        if (!this.loadedServerConfigs && !this.minecraft.hasSingleplayerServer()) { // Don't override actual configs with client dummy data for the local player
             ConfigTracker.INSTANCE.loadDefaultServerConfigs();
+            this.loadedServerConfigs = true; // This is to ensure the default configs are loaded before any config data is received
+        }
     }
 }
