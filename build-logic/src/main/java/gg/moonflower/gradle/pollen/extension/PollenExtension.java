5470808bc71b964f547c9230ef9e114e86207a4f
Jackson <jackson.hardaway@gmail.com>
2022-09-05 20:36:05 -0700

    Added module dependency

--- build-logic/src/main/java/gg/moonflower/gradle/pollen/extension/PollenExtension.java
+++ build-logic/src/main/java/gg/moonflower/gradle/pollen/extension/PollenExtension.java
@@ -2,24 +2,46 @@ package gg.moonflower.gradle.pollen.extension;
 
 import org.gradle.api.Project;
 import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ListProperty;
 import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 
 import javax.inject.Inject;
+import java.io.Serial;
+import java.io.Serializable;
 
-public class PollenExtension {
+public class PollenExtension implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 2L;
 
     private final Property<String> apiName;
+    private final ListProperty<String> moduleDependencies;
 
     @Inject
     public PollenExtension(ObjectFactory factory, Project project) {
         this.apiName = factory.property(String.class);
         this.apiName.finalizeValueOnRead();
+        this.moduleDependencies = factory.listProperty(String.class);
+        this.moduleDependencies.finalizeValueOnRead();
+    }
+
+    public void moduleDependency(String module) {
+        this.moduleDependencies.add(module);
     }
 
     @Input
+    public ListProperty<String> getModuleDependencies() {
+        return moduleDependencies;
+    }
+
+    public void setModuleDependencies(Iterable<String> list) {
+        this.moduleDependencies.set(list);
+    }
+
+
+    @Input
     public Property<String> getApiName() {
-        return this.apiName;
+        return apiName;
     }
 
     public void setApiName(String name) {
