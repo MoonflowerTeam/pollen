ebb772066ebd24499dfcc584e07dcfedecbeab93
Ocelot <FinntheRaider@gmail.com>
2023-01-14 15:35:16 -0700

    Added vanilla loot tables for validation

--- common/src/main/java/gg/moonflower/pollen/api/datagen/provider/loot_table/PollinatedLootTableProvider.java
+++ common/src/main/java/gg/moonflower/pollen/api/datagen/provider/loot_table/PollinatedLootTableProvider.java
@@ -1,11 +1,13 @@
 package gg.moonflower.pollen.api.datagen.provider.loot_table;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Multimap;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.HashCache;
+import net.minecraft.data.loot.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.LootTables;
@@ -17,11 +19,10 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * Generates block states and models for blocks and items.
@@ -33,6 +34,9 @@ public class PollinatedLootTableProvider implements DataProvider {
 
     private static final Logger LOGGER = LogManager.getLogger();
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
+
+    private static final List<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>> VANILLA_PROVIDERS = ImmutableList.
+            of(FishingLoot::new, ChestLoot::new, EntityLoot::new, BlockLoot::new, PiglinBarterLoot::new, GiftLoot::new);
 
     private final Map<LootContextParamSet, Set<PollinatedLootGenerator>> lootGenerators;
     private final DataGenerator generator;
@@ -56,7 +60,19 @@ public class PollinatedLootTableProvider implements DataProvider {
             lootGenerators.forEach(generator -> generator.accept(registry));
         });
 
-        ValidationContext validationContext = new ValidationContext(LootContextParamSets.ALL_PARAMS, resourceLocationx -> null, lootTables::get);
+        Map<ResourceLocation, LootTable> registry = new HashMap<>(lootTables);
+        try {
+            // Add vanilla block loot for validation
+            VANILLA_PROVIDERS.forEach(provider -> provider.get().accept((name, builder) -> {
+                if (registry.put(name, builder.build()) != null) {
+                    throw new IllegalStateException("Duplicate loot table " + name);
+                }
+            }));
+        } catch (Throwable ignored) {
+            // Don't care, didn't ask
+        }
+
+        ValidationContext validationContext = new ValidationContext(LootContextParamSets.ALL_PARAMS, resourceLocationx -> null, registry::get);
         lootTables.forEach((name, lootTable) -> LootTables.validate(validationContext, name, lootTable));
         Multimap<String, String> problems = validationContext.getProblems();
         if (!problems.isEmpty()) {
